e5be26a8e90113c2dd1775a3255e19dcc0a326db
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-13 19:47:10 +0100

    refactor comments test

--- server/test/comment.spec.js
+++ server/test/comment.spec.js
@@ -3,7 +3,7 @@ import chaiHttp from 'chai-http';
 import app from '../../index';
 
 chai.use(chaiHttp);
-let token;
+let userToken;
 let id;
 before(async () => {
   const userResponse = await chai
@@ -14,14 +14,14 @@ before(async () => {
       password: 'soldier123'
     });
   const response = userResponse.body.data;
-  ({ token } = response);
+  userToken = response.token;
 });
 
 describe('User post comment route', () => {
   it('should post a comment to an article', async () => {
     const res = await chai.request(app)
       .post('/api/v1/article/the-boy-drank-palm-wine-3456677788/comment')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: 'This is a valid comment' });
     ({ id } = res.body.data.comment);
     expect(res).to.have.status(201);
@@ -32,7 +32,7 @@ describe('User post comment route', () => {
   it('should return 422 when comment not sent', async () => {
     const res = await chai.request(app)
       .post('/api/v1/article/a11f440b-eae3-4d28-990d-700c7b965709/comment')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: ' ' });
 
     expect(res).to.have.status(422);
@@ -77,7 +77,7 @@ describe('User Threaded comments', () => {
   it('should post a comment to an article', async () => {
     const res = await chai.request(app)
       .post(`/api/v1/article/the-boy-drank-palm-wine-3456677788/comment/${id}/thread`)
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: 'This is a valid sub comment' });
     expect(res).to.have.status(201);
     expect(res).to.be.a('object');
@@ -87,7 +87,7 @@ describe('User Threaded comments', () => {
   it('should return 422 when comment on sent', async () => {
     const res = await chai.request(app)
       .post('/api/v1/article/833c0925-3dc5-424e-a575-a065eb71d5b3/comment/96de3cbe-9544-41a8-8f53-73d7e917c7b2/thread')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: ' ' });
 
     expect(res).to.have.status(422);
@@ -132,7 +132,7 @@ describe('User post highlighted text', () => {
   it('should return an error if highlighted text cannot be found in the article', async () => {
     const res = await chai.request(app)
       .post('/api/v1/article/Gildard is working on it-12345678/comment')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: 'This is a valid comment', highlightedText: 'hhhhh', spanId: 'span1' });
     expect(res).to.have.status(404);
     expect(res).to.be.a('object');
@@ -142,7 +142,7 @@ describe('User post highlighted text', () => {
   it('should post a comment on highlighted text of an article', async () => {
     const res = await chai.request(app)
       .post('/api/v1/article/Gildard is working on it-12345678/comment')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: 'This is a an ancient name', highlightedText: 'boy', spanId: 'span2' });
     expect(res).to.have.status(201);
     expect(res).to.be.a('object');
@@ -153,7 +153,7 @@ describe('User post highlighted text', () => {
   it('should return 400 error if spanid is not specified', async () => {
     const res = await chai.request(app)
       .post('/api/v1/article/Gildard is working on it-12345678/comment')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({ comment: 'This is a an ancient name', highlightedText: 'boy', spanId: '' });
     expect(res).to.have.status(400);
     expect(res).to.be.a('object');
@@ -165,7 +165,7 @@ describe('Edit comment', () => {
   it('should return 200 if article is successfully edited', async () => {
     const res = await chai.request(app)
       .put('/api/v1/article/Gildard is working on it-12345678/comment/bfee5ce0-d1fd-4ef3-83ce-07a03041c5e8')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({
         comment: 'Change this comment'
       });
@@ -179,7 +179,7 @@ describe('Edit comment', () => {
   it('should return error if incorrect id is passed', async () => {
     const res = await chai.request(app)
       .put('/api/v1/article/Gildard is working on it-12345678/comment/23')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({
         comment: 'Change this comment'
       });
@@ -191,7 +191,7 @@ describe('Edit comment', () => {
   it('should return error if comment is empty', async () => {
     const res = await chai.request(app)
       .put('/api/v1/article/gildard-is-working-on-it-12345678/comment/bfee5ce0-d1fd-4ef3-83ce-07a03041c5e8')
-      .set({ Authorization: `Bearer ${token}` })
+      .set({ Authorization: `Bearer ${userToken}` })
       .send({
         comment: ''
       });
@@ -216,7 +216,7 @@ describe('Delete comments', () => {
   it('should return 404 if the article does not exist', async () => {
     const res = await chai.request(app)
       .delete('/api/v1/article/wrong-slug/comment/1b030f7c-7387-4cdd-ae0a-913737c0f96e')
-      .set({ Authorization: `Bearer ${token}` });
+      .set({ Authorization: `Bearer ${userToken}` });
 
     expect(res).to.have.status(404);
     expect(res.body).to.be.a('object');
@@ -226,7 +226,7 @@ describe('Delete comments', () => {
   it('should return 404 if comment does not exist', async () => {
     const res = await chai.request(app)
       .delete('/api/v1/article/Gildard is working on it-12345678/comment/1b030f7c-7387-4cdd-ae0a-913737c0f9f4')
-      .set({ Authorization: `Bearer ${token}` });
+      .set({ Authorization: `Bearer ${userToken}` });
 
     expect(res).to.have.status(404);
     expect(res.body).to.be.a('object');
@@ -236,7 +236,7 @@ describe('Delete comments', () => {
   it('should delete a comment and thread comments', async () => {
     const res = await chai.request(app)
       .delete('/api/v1/article/the-boy-drank-palm-wine-3456677788/comment/f24afaca-a55f-44c3-9705-539f36fd8f45')
-      .set({ Authorization: `Bearer ${token}` });
+      .set({ Authorization: `Bearer ${userToken}` });
 
     expect(res).to.have.status(200);
     expect(res.body.message).to.equal('Comment deleted.');
