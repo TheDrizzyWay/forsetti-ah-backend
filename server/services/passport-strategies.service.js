4103135252d0bc63c8e899d25625472ccbd257a4
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-14 13:42:10 +0100

    refactor social callbacks

--- server/services/passport-strategies.service.js
+++ server/services/passport-strategies.service.js
@@ -1,39 +1,33 @@
 import { Strategy as FacebookStrategy } from 'passport-facebook';
 import { Strategy as TwitterStrategy } from 'passport-twitter';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import dotenv from 'dotenv';
+import {
+  facebookClientId, facebookClientSecret, backendUrl,
+  twitterAppId, twitterAppSecret, googleClientId,
+  googleClientSecret
+} from '../config/variables';
 import { UserController } from '../controllers';
 
 const { socialCallback } = UserController;
 
-dotenv.config();
-
-
-const {
-  FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, BACKEND_URL,
-  TWITTER_APP_ID, TWITTER_APP_SECRET, GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET
-} = process.env;
-
-
 const facebookSetup = {
-  clientID: FACEBOOK_CLIENT_ID,
-  clientSecret: FACEBOOK_CLIENT_SECRET,
-  callbackURL: `${BACKEND_URL}api/v1/auth/facebook/callback`,
+  clientID: facebookClientId,
+  clientSecret: facebookClientSecret,
+  callbackURL: `${backendUrl}api/v1/auth/facebook/callback`,
   profileFields: ['id', 'emails', 'displayName', 'photos'],
 };
 
 const twitterSetup = {
-  consumerKey: TWITTER_APP_ID,
-  consumerSecret: TWITTER_APP_SECRET,
-  callbackURL: `${BACKEND_URL}api/v1/auth/twitter/callback`,
+  consumerKey: twitterAppId,
+  consumerSecret: twitterAppSecret,
+  callbackURL: `${backendUrl}api/v1/auth/twitter/callback`,
   includeEmail: true,
 };
 
 const googleSetup = {
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: `${BACKEND_URL}api/v1/auth/google/callback`
+  clientID: googleClientId,
+  clientSecret: googleClientSecret,
+  callbackURL: `${backendUrl}api/v1/auth/google/callback`
 };
 
 const facebookStrategy = new FacebookStrategy(facebookSetup, socialCallback);
